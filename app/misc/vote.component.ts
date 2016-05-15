dfb267d8318c6c0fdbe4cd65bdf9f6e51f3ee35f
Abhishek Dutt <abhishekdutt@outlook.com>
2016-05-15 20:16:16 +0530

    Wip CSS

--- app/misc/vote.component.ts
+++ app/misc/vote.component.ts
@@ -9,14 +9,14 @@ import {Component, OnInit, EventEmitter} from 'angular2/core';
         <div class="row">
           <div class="col-xs-6 vote-col-up">
             <div (click)="upVotePost(_votee.id)">
-              <div [ngClass]="{userHasVoted: _votee.currentUserHasUpVoted}">
+              <div [ngClass]="{userHasVoted: _votee.currentUserHasUpVoted}"  class="vote up-vote">
               {{_votee.upvotes}} <i class="fa fa-arrow-up"></i>
               </div> 
             </div>
           </div>
           <div class="col-xs-6 vote-col-down">
             <div (click)="downVotePost(_votee.id)">
-              <div [ngClass]="{userHasVoted: _votee.currentUserHasDownVoted}">
+              <div [ngClass]="{userHasVoted: _votee.currentUserHasDownVoted}" class="vote down-vote">
                 <i class="fa fa-arrow-down"></i> {{_votee.downvotes}}
               </div> 
             </div>
@@ -27,19 +27,35 @@ import {Component, OnInit, EventEmitter} from 'angular2/core';
     </div>
   `,
   styles: [`
+    .my-vote {
+      margin-top: 10px;
+      font-size: 14px;
+      border-radius: 10px;
+      background-color: ghostwhite;
+      color: rgba(0, 0, 0, 0.3);
+    }
     .my-vote .vote-col-up {
       padding-right: 0;
     }
     .my-vote .vote-col-down {
       padding-left: 0;
     }
-    .my-vote {
-      font-size: 12px;
-      overflow-wrap: break-word;
-      word-wrap: break-word;
-    }
     .my-vote .userHasVoted {
-      background-color: aqua;    
+      background-color: floralwhite;
+      color: rgba(0, 0, 0, 0.8);  
+    }
+    .my-vote .vote {
+      height: 40px;
+      text-align: center;
+      padding-top: 9px;
+    }
+    .my-vote .up-vote {
+      border-top-left-radius: 10px;
+      border-bottom-left-radius: 10px;
+    }
+    .my-vote .down-vote {
+      border-top-right-radius: 10px;
+      border-bottom-right-radius: 10px;
     }
   `],
   inputs: ['_votee'],
--- app/post/post.component.ts
+++ app/post/post.component.ts
@@ -23,7 +23,7 @@ import {DateFormatPipe} from '../misc/date-format.pipe';
           <div class="row">
             <div class="col-xs-12">
               <a [routerLink]="['ViewPost', {postid: post.id}]" class="post-title">
-                <span>{{post.title}} </span> 
+                <span>{{post.title}}</span> 
               </a>
               <span *ngIf="post.type === 'link'"> 
                 <a target="_blank" [href]="post.link">[view link]</a>
@@ -31,38 +31,65 @@ import {DateFormatPipe} from '../misc/date-format.pipe';
             </div>
           </div>
           
-          <div class="row">
+          <div class="row" *ngIf="post.textTrimmed">
             <div class="col-xs-12">
-              {{post.text}}
+              <div class="post-text">
+                {{post.textTrimmed}}
+              </div>
+              <div class="read-more">
+                Read more ...
+              </div>
             </div>  
           </div>
           
           <div class="row">
             <div class="col-xs-12">
-              <div class="text-muted post-info">
-                <div>
-                  <a class="" [routerLink]="['ViewUser', {id: post.postedby.id}]">
-                    <i class="fa fa-user"></i> {{post.postedby.displayname}}
-                  </a> &bull; {{ post.createdAt | timeAgo }} &bull;
-                  <a class="" [routerLink]="['ViewGroup', {super_group_name: post.group.supergroup.name, group_name: post.group.name}]">
-                    go/{{post.group.supergroup.name}}/{{post.group.name}}
-                  </a>
-                </div>
-                <div>
+              <div class="post-info">
                 
-                  <div class="row">
-                    <div class="col-xs-6">
-                      {{post.comments.length}} Comments 
-                      <span *ngIf="currentUser && currentUser.id == post.postedby.id"> &bull; 
+                <div class="">
+                  
+                  <a class="" [routerLink]="['ViewUser', {id: post.postedby.id}]">
+                    <div class="profile-image pull-left">
+                      <i class="fa fa-user"></i>
+                    </div>
+                  </a>
+                  
+                  <div class="pull-left">
+                    <div class="profile-name pull-left">
+                      <a class="" [routerLink]="['ViewUser', {id: post.postedby.id}]">
+                        {{post.postedby.displayname}}
+                      </a>
+                    </div> 
+                    <div class="bullet">&bull;</div>
+                    <div class="post-grouplink">
+                      <a class="" [routerLink]="['ViewGroup', {super_group_name: post.group.supergroup.name, group_name: post.group.name}]">
+                        go/{{post.group.supergroup.name}}/{{post.group.name}}
+                      </a>
+                    </div>
+                    <div class="clearfix"></div>
+                    <div class="post-createdat pull-left"> 
+                      {{ post.createdAt | timeAgo }} &bull;
+                    </div>
+                    <div class="post-commentstotal pull-left">
+                      {{post.comments.length}} Comments &bull; &nbsp;
+                    </div>
+                    <div class="post-delete pull-left">
+                      <span *ngIf="currentUser && currentUser.id == post.postedby.id">
                         <a class="" [routerLink]="['ConfirmPostDelete', {postid: post.id}]"> Delete </a>
                       </span>
                     </div>
-                    <div class="col-xs-6">
+                  </div>
+                  
+                </div> 
+                
+                <div>
+                  <div class="row">
+                    <div class="col-xs-12">
                       <my-vote [_votee]='post' (upVote)='upVotePost($event)' (downVote)='downVotePost($event)'></my-vote>
                     </div>
                   </div>
-                  
                 </div>
+                
               </div>
             </div>
           </div>
@@ -90,7 +117,9 @@ import {DateFormatPipe} from '../misc/date-format.pipe';
           
           <div class="row">
             <div class="col-xs-12">
-              {{post.text}}
+              <div class="post-text">
+                {{post.textTrimmed}}
+              </div>
             </div>  
           </div>
           
@@ -141,7 +170,9 @@ import {DateFormatPipe} from '../misc/date-format.pipe';
             
             <div class="row">
               <div class="col-xs-12">
-                {{post.text}}
+                <div class="post-text">
+                  {{post.text}}
+                </div>
               </div>  
             </div>
             <div class="">
@@ -190,14 +221,111 @@ import {DateFormatPipe} from '../misc/date-format.pipe';
     margin-bottom: 5px;
   }
   .my-post .post-container .post-title {
-    margin-bottom: 5px;
-    font-weight: bold;
-    color: black;
+    color: rgba(0,0,0,.8);
+    font-family: "Lucida Grande", "Lucida Sans Unicode", "Lucida Sans", Geneva, Arial, sans-serif;
+    font-weight: 700;
+    font-style: normal;
+    font-size: 28px;
+    margin-left: -1.75px;
+    line-height: 1.12;
+    letter-spacing: -.022em;
+    word-wrap: break-word;
+  }
+  .my-post .post-container .post-text {
+    font-size: 24px;
+    margin-top: 8px;
+    margin-left: -1.5px;
+    line-height: 1.18;
+    letter-spacing: -.022em;
+    font-family: "Lucida Grande","Lucida Sans Unicode","Lucida Sans",Geneva,Arial,sans-serif;
+    font-weight: 300;
+    font-style: normal;
+    color: rgba(0,0,0,.44);
+    word-wrap: break-word;
+  }
+  .my-post .post-container .read-more {
+    color: rgba(0, 0, 0, 0.8);
+    display: block;
+    font-family: BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-size: 11px;
+    height: 25px;
+    line-height: 25.2px;
+    margin-top: 4px;
+    margin-bottom: 4px;
+    -webkit-font-smoothing: antialiased;
   }
   .my-post .post-info {
     font-size: 12px;
+    margin-top: 15px;
     overflow-wrap: break-word;
     word-wrap: break-word;
+  }
+  .my-post .post-container .profile-image {
+    height: 32px;
+    width: 32px;
+    border: 1px solid lightgrey;
+    margin-top: 3px
+  }
+  .my-post .post-container .profile-name {
+    cursor: pointer;
+    display: inline;
+    font-family: BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: normal;
+    height: auto;
+    letter-spacing: normal;
+    line-height: 19.6px;
+    text-decoration: none;
+    unicode-bidi: isolate;
+    visibility: visible;
+    white-space: nowrap;
+    width: auto;
+    -webkit-font-smoothing: antialiased;
+    -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+    padding-left: 10px;
+  }
+  .my-post .post-container .post-grouplink {
+    cursor: pointer;
+    display: inline;
+    font-family: BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: normal;
+    height: auto;
+    letter-spacing: normal;
+    line-height: 19.6px;
+    text-decoration: none;
+    unicode-bidi: isolate;
+    visibility: visible;
+    white-space: nowrap;
+    width: auto;
+    -webkit-font-smoothing: antialiased;
+    -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+  }
+  .my-post .post-container .post-createdat {
+    display: inline;
+    font-family: BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-size: 12px;
+    padding-left: 10px;
+    line-height: 14.4px;
+    color: rgba(0, 0, 0, 0.439216);
+  }
+  .my-post .post-container .post-commentstotal {
+    display: inline;
+    font-family: BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-size: 12px;
+    padding-left: 5px;
+    line-height: 14.4px;
+    color: rgba(0, 0, 0, 0.439216);
+  }
+  .my-post .post-container .bullet {
+    display: inline;
+    font-family: BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-size: 12px;
+    padding-left: 5px;
+    line-height: 14.4px;
+    color: rgba(0, 0, 0, 0.439216);
   }
   `],
   inputs: ['post', 'type', 'currentUser'],
@@ -223,6 +351,14 @@ export class PostComponent implements OnInit {
     this.templateTypeList = PostTemplateType.List;
     this.templateTypeGroupList = PostTemplateType.Grouplist;
     this.templateTypeMain = PostTemplateType.Main;
+    console.log("PSOT", this.post)
+    // I define this MAXCHARS var value by hunch
+    let MAXCHARS = 150;
+    if(this.post) this.post.readmore = false;
+    if(this.post && this.post.text.length > MAXCHARS) {
+      this.post.textTrimmed = this.post.text.substring(0, MAXCHARS) + ' ...';
+      this.post.readmore = true;
+    }
   }
   
   gotoPost(id: number) {
